8ee6be2707f6035155a3714c1aab8b1ffc820caa
VoxeveR <piotrzietek01@gmail.com>
2024-05-31 13:45:21 +0200

    message handling

--- src/main/java/com/app/chatapp/ChatSceneController.java
+++ src/main/java/com/app/chatapp/ChatSceneController.java
@@ -1,20 +1,19 @@
 package com.app.chatapp;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
-import javafx.stage.FileChooser;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -30,9 +29,22 @@ public class ChatSceneController implements Initializable {
     private Circle userImageCircle;
     @FXML
     private Button sendMessageButton;
+    @FXML
+    private TextField messageBox;
+    @FXML
+    private ScrollPane messageContainer;
+    @FXML
+    private VBox messageVBox;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+/*        Scene scene = userImageCircle.getScene();
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, keyEvent -> {
+            if(keyEvent.getCode() == KeyCode.ENTER) {
+                sendMessage();
+            }
+        });*/
         FileInputStream inputstream = null;
         try {
             inputstream = new FileInputStream("./src/main/resources/com/app/chatapp/pictures/avatar.jpg");
@@ -62,4 +74,15 @@ public class ChatSceneController implements Initializable {
     }
 
 
+    public void sendMessage() {
+
+        String message = messageBox.getText();
+        Label label = new Label(message);
+        messageVBox.getChildren().add(label);
+        messageContainer.setContent(messageVBox);
+
+        messageContainer.setFitToHeight(true);
+        messageContainer.setFitToWidth(true);
+        messageContainer.vvalueProperty().bind(messageVBox.heightProperty());
+    }
 }
