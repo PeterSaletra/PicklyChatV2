a586eec561e0c84b1f85d4cc55a362549081878d
newmon13 <kubalipka123@gmail.com>
2024-06-08 19:39:53 +0200

    Added profantiy filter, component sends HTTP request to api.moderatecontent.com/text and api returns JSON containing cleaned up message.

---
+++ src/main/java/com/app/chatapp/filter/ProfanityFilter.java
@@ -0,0 +1,78 @@
+package com.app.chatapp.filter;
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class ProfanityFilter {
+    private final String apiKey = "[5092d3a8b925b1c6a90e6791fdf9f1bd]";
+    private final String url = "https://api.moderatecontent.com/text/?";
+    private String replace;
+    public ProfanityFilter(char replacementChar){
+        replace = String.valueOf(replacementChar);
+    }
+    String filterMessage(String message) {
+        JSONObject reqJSON = new JSONObject();
+        try{
+            reqJSON.put("msg", URLEncoder.encode(message, "UTF-8"));
+            reqJSON.put("key",  URLEncoder.encode(apiKey, "UTF-8"));
+            reqJSON.put("replace", replace);
+
+            String urlParameters = getUrlParams(reqJSON);
+            System.out.println("GIT: " + urlParameters);
+
+            URI uri = new URI(url + urlParameters);
+            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("accept", "application/json");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            JSONParser parser = new JSONParser();
+            JSONObject resJSON = (JSONObject) parser.parse(response.toString());
+
+            return resJSON.get("clean").toString();
+
+        }catch (UnsupportedEncodingException err){
+            err.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return message;
+    }
+
+    private static String getUrlParams(JSONObject json){
+        StringBuilder urlParameters = new StringBuilder();
+        ArrayList<String> keys = new ArrayList<>();
+        keys.addAll(json.keySet());
+        Iterator<String> iter = keys.iterator();
+        while(iter.hasNext()){
+            String key = iter.next();
+            urlParameters.append(key + "=" + json.get(key) + (iter.hasNext() ? "&" : ""));
+        }
+        return urlParameters.toString();
+    }
+
+    public static void main(String[] args){
+        ProfanityFilter filter = new ProfanityFilter('*');
+        System.out.println(filter.filterMessage("kurwy mać jebaniec suka pierdolony łąka"));
+    }
+
+
+
+
+
+
+}
